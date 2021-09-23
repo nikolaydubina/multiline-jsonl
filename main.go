679f4d707ec3a57ee17aa03911953cd40a3fcfd2
Nikolay <nikolay.dubina.pub@gmail.com>
2021-09-23 08:22:20 +0800

    fix doc

--- main.go
+++ main.go
@@ -13,14 +13,13 @@ func main() {
 	var (
 		expand bool
 	)
-
-	flag.BoolVar(&expand, "expand", false, "if true will pretty print each JSON")
-	flag.Parse()
 	flag.Usage = func() {
 		doc := "This tool reads multiple JSONs from STDIN and writes formatted output to STDOUT.\n\nUsage of %s:\n"
 		fmt.Fprintf(flag.CommandLine.Output(), doc, os.Args[0])
 		flag.PrintDefaults()
 	}
+	flag.BoolVar(&expand, "expand", false, "if true will pretty print each JSON")
+	flag.Parse()
 
 	if err := mjsonl.FormatJSONL(os.Stdin, os.Stdout, expand); err != nil {
 		log.Fatal(err.Error())
